5768b1ae6a06ebdaad3b96a8e31bdd9490516f4f
yingyingzhu <yingyingzhu@sohu-inc.com>
2023-03-17 09:30:10 +0800

    更新为pytestt

--- test_binary_search.py
+++ test_binary_search.py
@@ -1,4 +1,7 @@
 # 二分查找法的逻辑
+
+import pytest
+
 """
 时间复杂度：对于存在列表中的元素，查找最多需要log2N而对于不再列表中的元素则需要log2N+1,原因是因为在列表中时，则需要将最终的元素也进行对比
 """
@@ -22,9 +25,19 @@ def binary_search(lo, item):
     return None
 
 
-if __name__ == '__main__':
-    lo = [1, 2, 3, 4, 5, 6, 7, 8]
 
-    k, i = binary_search(lo, 8)
+
+
+def test_binary_search():
+    lo = [1, 2, 3, 4, 5, 6, 7, 8]
+    item = 8
+    k, i = binary_search(lo, item)
 
     print(k, i)
+
+# if __name__ == '__main__':
+#     lo = [1, 2, 3, 4, 5, 6, 7, 8]
+#
+#     k, i = binary_search(lo, 8)
+#
+#     print(k, i)
